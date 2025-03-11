edef2ecc4fc409c642e55264c275625f0ae57e3c
tommmasotacconi <tommaso.tacconi@gmail.com>
2025-03-11 12:57:44 +0100

    Added js injected calendar layout with style

--- jscript/script.js
+++ jscript/script.js
@@ -1,1 +1,23 @@
 console.log('js ok');
+
+// SET UP PHASE
+const main = document.querySelector('main');
+const row = document.createElement('div');
+row.classList.add('row'); 
+
+console.log(main, source);
+
+//PROCESSING PHASE
+//Append 25 cards to row after generating one col div and one card div
+for (let i = 0; i < 25; i++) {
+	const col = document.createElement('div');
+	col.classList.add('col');
+	const card = document.createElement('div');
+	card.classList.add('calendar-cell');
+	card.innerHTML = i + 1;
+	col.appendChild(card);
+	row.appendChild(col);
+};
+
+// Append row div in page
+main.appendChild(row);
